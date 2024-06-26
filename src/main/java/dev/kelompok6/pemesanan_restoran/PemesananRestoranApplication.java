29bbe120ed18c6518cc50344660d13e951487f61
Ryu K <ryuuwiz@gmail.com>
2024-06-26 19:45:46 +0700

    chore: initial setup

---
+++ src/main/java/dev/kelompok6/pemesanan_restoran/PemesananRestoranApplication.java
@@ -0,0 +1,13 @@
+package dev.kelompok6.pemesanan_restoran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PemesananRestoranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PemesananRestoranApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kelompok6/pemesanan_restoran/PemesananRestoranApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kelompok6.pemesanan_restoran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PemesananRestoranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
